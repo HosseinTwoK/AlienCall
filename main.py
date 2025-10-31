b2fe83a0033d02765f51a9211dee8539851d37dd
HosseinTwoK <78283760+HosseinTwoK@users.noreply.github.com>
2025-10-31 11:31:00 +0330

    Disable background music functionality
    Commented out background music loading and playing logic.

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ icon = pygame.image.load(path.join("assets","sprites","player.png")).convert_alp
 pygame.display.set_caption("Alien Call")
 pygame.display.set_icon(icon)
 
-pygame.mixer.music.load(path.join("assets","sounds","bgmusic.wav"))
-pygame.mixer.music.set_volume(0.3)
+#pygame.mixer.music.load(path.join("assets","sounds","bgmusic.wav"))
+#pygame.mixer.music.set_volume(0.3)
 
 clock = pygame.time.Clock()
 start = Start(screen = display_surface)
@@ -39,14 +39,15 @@ while running:
 
     running = game.update(key)
 
-    if guide_shown and not bgmusic_play:
-        pygame.mixer.music.play(-1,0.0)
-        bgmusic_play = True
+#    if guide_shown and not bgmusic_play:
+#        pygame.mixer.music.play(-1,0.0)
+#        bgmusic_play = True
     if not guide_shown:
         guide_shown = True
         running = guide_man.show_guide()
     
     
     clock.tick(FPS)
+
 
 pygame.quit()

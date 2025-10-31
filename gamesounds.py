6aa1e3d3bb685a7ce119e2ef83d51cd1779b2c84
HosseinTwoK <78283760+HosseinTwoK@users.noreply.github.com>
2025-10-31 11:29:46 +0330

    Refactor sound file paths in gamesounds.py
    Updated sound file paths to remove 'assets/' prefix.

--- gamesounds.py
+++ gamesounds.py
@@ -4,26 +4,27 @@ from os import path
 
 pygame.init()
 # done
-soundGuideManLoad = pygame.mixer.Sound(path.join("assets","sounds","menu-load.wav"))
+soundGuideManLoad = pygame.mixer.Sound(path.join("sounds","menu-load.wav"))
 soundGuideManLoad.set_volume(0.2)
 
-soundGameOver = pygame.mixer.Sound(path.join("assets","sounds","game-over.wav"))
+soundGameOver = pygame.mixer.Sound(path.join("sounds","game-over.wav"))
 soundGameOver.set_volume(0.1)
 
 # done
-soundPlayerShoot = pygame.mixer.Sound(path.join("assets","sounds","player-shoot.wav"))
+soundPlayerShoot = pygame.mixer.Sound(path.join("sounds","player-shoot.wav"))
 soundPlayerShoot.set_volume(0.1)
 
 # done
-soundPlayerDamaged = pygame.mixer.Sound(path.join("assets","sounds","player-damaged.wav"))
+soundPlayerDamaged = pygame.mixer.Sound(path.join("sounds","player-damaged.wav"))
 soundPlayerDamaged.set_volume(0.1)
 
 # done
-soundPowerUp = pygame.mixer.Sound(path.join("assets","sounds","powerup.wav"))
+soundPowerUp = pygame.mixer.Sound(path.join("sounds","powerup.wav"))
 soundPowerUp.set_volume(0.1)
 
 # done
-soundAlienShoot = pygame.mixer.Sound(path.join("assets","sounds","alien-shoot.wav"))
+soundAlienShoot = pygame.mixer.Sound(path.join("sounds","alien-shoot.wav"))
 soundAlienShoot.set_volume(0.1)
+
 
 pygame.quit()
